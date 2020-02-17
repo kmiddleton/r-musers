2b236d671833d976faa811daeb4be7ae4acc355f
Kevin Middleton <middletonk@missouri.edu>
2020-02-17 14:21:41 -0600

    Add files

---
+++ sticker.R
@@ -0,0 +1,28 @@
+library(hexSticker)
+library(ggplot2)
+library(showtext)
+library(ggridges)
+library(wesanderson)
+
+## Loading Google fonts (http://www.google.com/fonts)
+font_add_google("Anton", "anton")
+
+## Automatically use showtext to render text for future devices
+showtext_auto()
+
+p <- ggplot(mtcars, aes(x = mpg, y = factor(cyl), fill = factor(cyl))) +
+  scale_fill_manual(values = wes_palette("Zissou1", 3)) +
+  geom_density_ridges(scale = 5, alpha = 0.5) +
+  theme_void() + theme_transparent() +
+  theme(legend.position = "none")
+
+s <- sticker(p, package = "R MUSers",
+        p_size = 30,
+        s_x = 1, s_y = 0.75, s_width = 1, s_height = 1,
+        p_family = "anton",
+        p_color = "gold",
+        h_fill="#000000",
+        h_color="gold")
+quartz()
+X11()
+print(s)
